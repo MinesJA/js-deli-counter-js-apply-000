c2c807145e04af462b25f5564d52502c682711c8
Jonathan Mines <minesja@gmail.com>
2017-11-10 12:32:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ var katzDeli = []
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name)
 
-  var number = katzDeliLine.length + 1
+  var number = katzDeliLine.length
 
   return `Welcome, ${name}. You are number ${number} in line.`
 }
 
 function nowServing(katzDeliLine){
   if(katzDeliLine.length > 0){
-    return `Now serving ${katzDeliLine.shift()}.`
+    return `Currently serving ${katzDeliLine.shift()}.`
   } else {
-    return "There is nobody waiting to be served."
+    return "There is nobody waiting to be served!"
   }
 }
 
